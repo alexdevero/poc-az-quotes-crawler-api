546a8909374de95c25013ce86dcb71ebe6af9a9f
Alex Devero <deveroalex@gmail.com>
2022-06-27 14:28:11 +0200

    Add author-pagination endpoint

--- api/index.ts
+++ api/index.ts
@@ -2,16 +2,34 @@ import express, { Request, Response } from 'express'
 import helmet from 'helmet'
 import compression from 'compression'
 
+import { getQuotesByAuthorPagination } from './../src/crawler'
+
 const app = express()
 app.use(helmet())
 app.disable('x-powered-by')
 app.use(compression())
 
 app.get('/api', async (req: Request, res: Response) => {
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.json({
     message: 'hello'
   })
+})
+
+app.get('/api/author-pagination', async (req: Request, res: Response) => {
+  try {
+    const { params } = req
+    const paginationData = await getQuotesByAuthorPagination(params.author)
+
+    res.json({
+      author: params.author,
+      numberOfPages: paginationData?.numberOfPages || 0
+    })
+  }
+  catch (e) {
+    res.json({
+      message: 'Sorry'
+    })
+  }
 })
 
 module.exports = app
