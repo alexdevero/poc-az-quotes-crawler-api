03f7d555acb9907656bcd1112ef4097444a21481
Alex Devero <deveroalex@gmail.com>
2022-06-27 14:20:02 +0200

    Add helmet, compression, use async

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express'
+import helmet from 'helmet'
+import compression from 'compression'
 
 const app = express()
+app.use(helmet())
+app.disable('x-powered-by')
+app.use(compression())
 
-app.get('/api', (req: Request, res: Response) => {
+app.get('/api', async (req: Request, res: Response) => {
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.json({
     message: 'hello'
