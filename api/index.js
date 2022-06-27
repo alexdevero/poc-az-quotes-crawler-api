f0421e3535583e4d5c8eaa595cf415ac863cb99d
Alex Devero <deveroalex@gmail.com>
2022-06-27 13:59:19 +0200

    Remove uuid call from api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
 const app = require('express')
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`
-  res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.json({
     message: 'hello'
