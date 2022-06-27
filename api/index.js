341b696275c67adc08c6f46d640fa0a7fb6b770a
Alex Devero <deveroalex@gmail.com>
2022-06-27 13:51:31 +0200

    Add dummy server (test)

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const app = require('express')
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.json({
+    message: 'hello'
+  })
+})
+
+module.exports = app
